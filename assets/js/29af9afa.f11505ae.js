79192e0e5aebe7e4e39bd02a8fc08375f6d87a1c
selgk <selcuksmtp1@gmail.com>
2025-03-01 01:15:17 +0300

    Deploy website - based on 25845d9abf239e0857be042dc2c7c4446f7e2156

---
+++ assets/js/29af9afa.f11505ae.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[4904],{9241:e=>{e.exports=JSON.parse('{"tag":{"label":"QML","permalink":"/blog/tags/qml","allTagsPath":"/blog/tags","count":2,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/qml","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
